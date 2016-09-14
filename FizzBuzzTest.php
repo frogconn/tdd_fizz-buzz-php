722252ac061c8885a5e17251c618581346278118
frogconn <frogconn@gmail.com>
2016-09-14 09:39:35 +0700

    Add test cass should be two.

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -5,15 +5,22 @@ class FizzBuzz
 
     public function count($number)
     {
-        return 1;
+        return $number;
     }
 }
 
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
+
+	public function testSholdBeTwo(){
+		$fizzbuzz = new FizzBuzz();
+		$this->assertEquals("2",$fizzbuzz->count(2));
+	}
+
     public function testShouldBeOne()
     {
         $fizzbuzz = new FizzBuzz();
         $this->assertEquals("1", $fizzbuzz->count(1));
     }
+
 }
