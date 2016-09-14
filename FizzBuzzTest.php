77e1d60ee2cbec576eeeff1f97eefa69ac6b91f9
frogconn <frogconn@gmail.com>
2016-09-14 09:37:18 +0700

    Add should be one test case.

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -1,6 +1,19 @@
 <?php
-class FizzBuzzTest extends PHPUnit_Framework_TestCase {
-	
+
+class FizzBuzz
+{
+
+    public function count($number)
+    {
+        return 1;
+    }
 }
 
-
+class FizzBuzzTest extends PHPUnit_Framework_TestCase
+{
+    public function testShouldBeOne()
+    {
+        $fizzbuzz = new FizzBuzz();
+        $this->assertEquals("1", $fizzbuzz->count(1));
+    }
+}
