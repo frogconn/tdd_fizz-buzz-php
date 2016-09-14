0f6d038d1bf26bcad80b856f3b31655d99320f1c
frogconn <frogconn@gmail.com>
2016-09-14 09:48:51 +0700

    Added should be "Buzz" with parameter number 5;

--- FizzBuzzTest.php
+++ FizzBuzzTest.php
@@ -5,29 +5,41 @@ class FizzBuzz
 
     public function count($number)
     {
-    	if($number == 3){
-    		return "Fizz";
-    	}
+        if ($number == 3) {
+            return "Fizz";
+        } elseif ($number == 5) {
+            return "Buzz";
+        }
         return $number;
     }
 }
 
 class FizzBuzzTest extends PHPUnit_Framework_TestCase
 {
-	public function testSholdBeFour(){
-		$fizzbuzz = new FizzBuzz();
-		$this->assertEquals("4",$fizzbuzz->count(4));
-	}
 
-	public function testSholdBeFizzWithThree(){
-		$fizzbuzz = new FizzBuzz();
-		$this->assertEquals("Fizz",$fizzbuzz->count(3));
-	}
+    public function testSholdBeBuzzWithFive()
+    {
+        $fizzbuzz = new FizzBuzz();
+        $this->assertEquals("Buzz", $fizzbuzz->count(5));
+    }
 
-	public function testSholdBeTwo(){
-		$fizzbuzz = new FizzBuzz();
-		$this->assertEquals("2",$fizzbuzz->count(2));
-	}
+    public function testSholdBeFour()
+    {
+        $fizzbuzz = new FizzBuzz();
+        $this->assertEquals("4", $fizzbuzz->count(4));
+    }
+
+    public function testSholdBeFizzWithThree()
+    {
+        $fizzbuzz = new FizzBuzz();
+        $this->assertEquals("Fizz", $fizzbuzz->count(3));
+    }
+
+    public function testSholdBeTwo()
+    {
+        $fizzbuzz = new FizzBuzz();
+        $this->assertEquals("2", $fizzbuzz->count(2));
+    }
 
     public function testShouldBeOne()
     {
